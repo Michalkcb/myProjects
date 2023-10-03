14b30ab4fb7b33b8fa5faba302b3e863926e1283
Michalkcb <michalkcb@gmail.com>
2023-10-03 22:53:04 +0200

    add remove li function

--- theSimplestMemoryGame/toDoApp1/app.js
+++ theSimplestMemoryGame/toDoApp1/app.js
@@ -1,6 +1,12 @@
-document.querySelectorAll('li')
- .forEach(function(li) {
-    li.addEventListener('click', function(e) {
-      e.target.classList.toggle('selected');
-    });
-  });
+const removeTask = (e) => {
+    // e.target.remove();
+    // e.target.parentNode.remove();
+    // console.log(e.target.parentNode);
+    // e.target.parentNode.style.textDecoration = "line-through";
+    // e.target.remove();
+    const index = e.target.dataset.key;
+    document.querySelector(`li[data-key="${index}"]`).remove()
+   
+   }
+   
+   document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask))
