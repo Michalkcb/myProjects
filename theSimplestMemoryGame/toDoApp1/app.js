1b0c73615f17b7d3a6b3733ed951066ec31da8f0
Michalkcb <michalkcb@gmail.com>
2023-10-04 20:30:14 +0200

    add added

--- theSimplestMemoryGame/toDoApp1/app.js
+++ theSimplestMemoryGame/toDoApp1/app.js
@@ -1,45 +1,22 @@
-//Usuwanie elementów li
-const removeTask = (e) => {
-    // e.target.remove();
-    // e.target.parentNode.remove();
-    // console.log(e.target.parentNode);
-    // e.target.parentNode.style.textDecoration = "line-through";
-    // e.target.remove();
-    const index = e.target.dataset.key;
-    document.querySelector(`li[data-key="${index}"]`).style.textDecoration = "line-through";
-    e.target.remove();
-   }
-   
-   document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask))
 
-   //wysukiwanie elementów li
-   const arr = [3,134134,1341,14,14,1434,145,2];
-   
-   //filter
-   const oddNumbers = arr.filter((number) => number%2);
-   const evenNumbers = arr.filter((number) => !(number%2));
-   const biggerThan = arr.filter((number) => number > 100);
-   // map
-   const doubleNumbers = arr.map((number) => number*2);
-   const people = arr.map(number => number / 2 + ' osob')
-   // forEach
-   arr.forEach((number, index) => arr[index] = number * 2)
 
-   //search
-   const input = document.querySelector('input');
-   const ul = document.querySelector('ul');
-   const liElements = document.querySelectorAll('li');
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const taskNumber = document.querySelector('h2 span');
+const listItems = document.querySelectorAll('li.task');
+const input = document.querySelector('input');
 
-   const searchTask = (e) => {
-    const searchText = e.target.value.toLowerCase();
-    let tasks = [...liElements];
-    tasks = tasks.filter((task) => task.textContent.toLowerCase().includes(searchText)); 
-   console.log(tasks);
-   ul.textContent = '';
-   tasks.forEach((task) => ul.appendChild(task));
+//dodawanie zadania
+const addTask = (e) => {
+  e.preventDefault();
+  const titleTask = input.value;
+  if (titleTask === '') return; 
+  const task = document.createElement('li');
+
+  task.className = 'task';
+  task.textContent = titleTask;
+  task.innerHTML = titleTask + "<button>Usuń</button>";
+  ul.appendChild(task);
+  input.value = '';
 }
-   // 
-
-   input.addEventListener('input', searchTask)
-
-   //add task
+form.addEventListener('submit', addTask);
