56a1bf0443025b11f98dfd6c3697929df4b67f9c
Michalkcb <michalkcb@gmail.com>
2023-10-05 14:13:11 +0200

    dark mode finish

--- darkMode/app.js
+++ darkMode/app.js
@@ -1,2 +1,11 @@
 const darkMode = document.getElementById('darkMode')
 const wrapper = document.getElementById('wrapper')
+
+//toggle to dark mode with css class prepared
+
+const toggleMode =(e) => {
+    e.preventDefault();
+    wrapper.classList.toggle('dark-mode');    
+}
+
+darkMode.addEventListener('click', toggleMode)
