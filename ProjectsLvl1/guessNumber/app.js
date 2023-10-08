63128b73da9de57a31a9903dd0608948fa95d4b5
Michalkcb <michalkcb@gmail.com>
2023-10-08 16:55:25 +0200

    finish guessNumber

--- ProjectsLvl1/guessNumber/app.js
+++ ProjectsLvl1/guessNumber/app.js
@@ -1,4 +1,5 @@
-const guessNumber = Math.floor(Math.random() * 100);
+const targetNumber = Math.floor(Math.random() * 100);
+console.log(targetNumber);
 
 const guessInput = document.getElementById("guessInput");
 const guessButton = document.getElementById("guessButton");
@@ -7,4 +8,18 @@ const message = document.getElementById("message");
 let attempts = 0;
 
 guessButton.addEventListener("click", () => {
+    const userGuess = parseInt(guessInput.value);
+  if (isNaN(userGuess) || userGuess < 0 || userGuess>99) {
+    message.textContent = "Podaj liczbę od 0 do 99.";
+  } else {
+    attempts++;
+    if (userGuess == targetNumber) {
+      message.textContent = `Brawo! Zgadłeś liczbę ${targetNumber} w ${attempts} próbach.`;
+    } else if (userGuess < targetNumber) {
+      message.textContent = "Szukana liczba jest większa.";
+    } else {
+      message.textContent = "Szukana liczba jest mniejsza.";
+    }
+  }
+  guessInput.value = "";
 });
