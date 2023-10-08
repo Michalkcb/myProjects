baaa5b515390464fb7257ea95d4bcf94ba4f116f
Michalkcb <michalkcb@gmail.com>
2023-10-08 16:26:41 +0200

    init guessNumber game

---
+++ ProjectsLvl1/guessNumber/app.js
@@ -0,0 +1,10 @@
+const guessNumber = Math.floor(Math.random() * 100);
+
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const message = document.getElementById("message");
+
+let attempts = 0;
+
+guessButton.addEventListener("click", () => {
+});
