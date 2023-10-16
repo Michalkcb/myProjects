f4e279cbad0c7b582972ffaee77c070eee3a2f86
Michalkcb <michalkcb@gmail.com>
2023-10-16 18:50:39 +0200

    add Reducer

--- ProjectsLevel3/react_calculator/src/App.js
+++ ProjectsLevel3/react_calculator/src/App.js
@@ -1,11 +1,16 @@
+import { useReducer } from 'react';
 import './style.css';
 
+function reducer(state, action) {
+}
+
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer => {})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
@@ -21,6 +26,7 @@ function App() {
       <button>7</button>
       <button>8</button>
       <button>9</button>
+      <button>-</button>
       <button>.</button>
       <button>0</button>
       <button className="span-two">=</button>
