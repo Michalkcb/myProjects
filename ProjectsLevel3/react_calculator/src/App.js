796371156ecb7e3f120854558d709ed1d684ae7f
Michalkcb <michalkcb@gmail.com>
2023-10-17 11:01:06 +0200

    repair DigitButton.js functions

--- ProjectsLevel3/react_calculator/src/App.js
+++ ProjectsLevel3/react_calculator/src/App.js
@@ -1,11 +1,31 @@
 import { useReducer } from 'react';
+import DigitButton from './DigitButton';
 import './style.css';
 
-function reducer(state, action) {
+export const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CHOOSE_OPERATION: 'choose-operation',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    EVALUATE: 'evaluate',
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state, 
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+    } 
+  }
 }
 
 function App() {
-  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer => {})
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,
+    {}
+    )
+
+
   return (
     <div className="calculator-grid">
       <div className="output">
@@ -14,7 +34,7 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>/</button>
+      <DigitButton digit='/' dispatch={dispatch}/>
       <button>1</button>
       <button>2</button>
       <button>3</button>
---
+++ ProjectsLevel3/react_calculator/src/DigitButton.js
@@ -0,0 +1,10 @@
+import { ACTIONS } from './App'
+
+export default function DigitButton({dispatch, digit}) {
+    return (    <button 
+    onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit} })}
+    >
+        {digit}
+        </button>
+)
+}
