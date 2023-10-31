7eaab1fcd2b60e1c26934bd343ba892ed6a1e7fc
Michalkcb <michalkcb@gmail.com>
2023-10-31 21:35:27 +0100

    add menulist

--- react/react01/src/pages/Menu.js
+++ react/react01/src/pages/Menu.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+        <h1 className='menuTitle'>Our Menu</h1>
+        <div className='menuList'></div>
+    </div>
   )
 }
 
