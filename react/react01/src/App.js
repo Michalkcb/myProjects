bf4fc9335837c06fee2b261d5853a9fc0f909495
Michalkcb <michalkcb@gmail.com>
2023-10-31 22:30:24 +0100

    finished page

--- react/react01/src/App.js
+++ react/react01/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
+import Contact from './pages/Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' ;
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/menu" exact element={<Menu />} />
         <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
     </Routes> 
     <Footer />
 </Router>
