8e6a46801469c8cf7aa1c93cb2a7a244ff29ee35
Michalkcb <michalkcb@gmail.com>
2023-10-31 22:16:17 +0100

    init Contact

---
+++ react/react01/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='contact'>
+        <div className='leftSide' style={{backgroundImage:`url(${MultiplePizzas})`} }></div>
+        <div className='rightSide'></div>
+    </div>
+  )
+}
+
+export default Contact
