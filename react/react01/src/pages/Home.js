56d3b02be1a2078b29f3b614c6de65e6215bc477
Michalkcb <michalkcb@gmail.com>
2023-10-31 21:07:44 +0100

    add home backgrount and text

--- react/react01/src/pages/Home.js
+++ react/react01/src/pages/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import BannerImage from './../assets/pizza.jpeg'
 
 function Home() {
   return (
-    <div>hello world</div>
-  )
+    <div className='home'>
+        <div className='headerContainer' style={{backgroundImage:`url(${BannerImage})`} }>
+            <h1>Perdo's Pizzeria</h1>
+            <p>PIZZA TO FIT ANY TASTE</p>
+            <Link to='/menu'>
+            <button> ORDER NOW </button>
+            </Link>
+        </div>
+    </div>
+  );
 }
 
 export default Home
