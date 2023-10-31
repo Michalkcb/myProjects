4fce6b495eb08c3038087d72632584c24dbb19e7
Michalkcb <michalkcb@gmail.com>
2023-10-31 19:02:24 +0100

    reinit react01

--- react/react01/src/App.js
+++ react/react01/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' ;
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-
+      <Router>
+<Navbar />
+    <Routes> 
+        <Route path="/" exact element={<Home />} />
+    </Routes> 
+</Router>
     </div>
   );
 }
--- react/react01/src/components/Navbar.js
+++ react/react01/src/components/Navbar.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import Logo from '../assets/pizzaLogo.png'
+import Logo from './../assets/pizzaLogo.png'
 
 function Navbar() {
   return (
     <div className='navbar'>
-        <div className='leftSide'>
-            <img src={Logo} />
-            </div>
-            <div className='rightSide'>
-            </div>
-
-
+        <div className='leftSide'> 
+        <img src={Logo}/>
+        </div>
+        <div className='rightSide'> </div>
     </div>
   )
 }
 
 export default Navbar
-
