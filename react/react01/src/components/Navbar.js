95450f7be52a5d583ca7594e47733ed38354c95d
Michalkcb <michalkcb@gmail.com>
2023-10-31 19:20:18 +0100

    add styles files

--- react/react01/src/components/Navbar.js
+++ react/react01/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from './../assets/pizzaLogo.png'
+import {Link} from 'react-router-dom'
+import '../styles/Navbar.css'
 
 function Navbar() {
   return (
@@ -7,7 +9,12 @@ function Navbar() {
         <div className='leftSide'> 
         <img src={Logo}/>
         </div>
-        <div className='rightSide'> </div>
+        <div className='rightSide'> 
+        <Link to='/'>Home</Link>
+        <Link to='/menu'>Menu</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+        </div>
     </div>
   )
 }
