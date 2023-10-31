7eeb5ed473a9e043573ff6f2f0897c4f60175c45
Michalkcb <michalkcb@gmail.com>
2023-10-31 22:14:18 +0100

    finish Abaut

--- react/react01/src/App.js
+++ react/react01/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import About from './pages/About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' ;
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Routes> 
         <Route path="/" exact element={<Home />} />
         <Route path="/menu" exact element={<Menu />} />
+        <Route path="/about" exact element={<About />} />
     </Routes> 
     <Footer />
 </Router>
