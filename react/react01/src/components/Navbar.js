a4ae35d8447502f5f28d0ebd99fb8fe3ee43d9d1
Michalkcb <michalkcb@gmail.com>
2023-10-31 16:21:37 +0100

    init react01

---
+++ react/react01/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../assets/pizzaLogo.png'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'>
+            <img src={Logo} />
+            </div>
+            <div className='rightSide'>
+            </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
+
