8a9dd3364a3d8357db59e2b159cd915b1f588f3d
Michalkcb <michalkcb@gmail.com>
2023-11-06 18:09:23 +0100

    menu order change

--- react/react01/src/helpers/MenuList.js
+++ react/react01/src/helpers/MenuList.js
@@ -6,35 +6,11 @@ import Pineapple from '../assets/pineapple.jpg'
 import Expensive from '../assets/expensive.jpg'
 
 export const MenuList = [
-    {
-      name: "Pepperoni Pizza",
-      image: Pepperoni,
-      price: 35.99,
-    },
-    {
-      name: "Margherita Pizza",
-      image: Margherita,
-      price: 31.99,
-    },
-    {
-      name: "Quadro fromaggie",
-      image: PedroTechSpecial,
-      price: 58.99,
-    },
-    {
-      name: "Vegan Pizza",
-      image: Vegan,
-      price: 37.99,
-    },
-    {
-      name: "Pineapple Pizza",
-      image: Pineapple,
-      price: 32.99,
-    },
-    {
-      name: "Special Pizza",
-      image: Expensive,
-      price: 59.99,
-    },
+    {name: "Margherita Pizza", image: Margherita, price: 31.99,},
+    {name: "Hawaiian Pizza", image: Pineapple, price: 32.99,},
+    {name: "Vegan Pizza", image: Vegan, price: 37.99,},
+    {name: "Pepperoni Pizza", image: Pepperoni, price: 35.99,},
+    {name: "Quadro fromaggie", image: PedroTechSpecial, price: 58.99,},
+    {name: "Special Pizza", image: Expensive, price: 59.99,},
   ];
 
