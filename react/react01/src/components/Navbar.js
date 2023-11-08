35e54d87177d8cbfe600643f30b5dd0b331dfcaf
Michalkcb <michalkcb@gmail.com>
2023-11-08 15:24:43 +0100

    no changes

--- react/react01/src/components/Navbar.js
+++ react/react01/src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <div className='navbar'>
         <div className='leftSide' id={openLinks ? 'open' : 'close'}> 
-        <img src={Logo}/>
+        <img src={Logo} alt=""/>
         <div className='hiddenLinks'>
         <Link to='/'>Home</Link>
         <Link to='/menu'>Menu</Link>
